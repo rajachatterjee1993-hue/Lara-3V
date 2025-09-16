a01b3723bc5f411646c1a04cf856d39260ecbede
rajachatterjee1993-hue <rajachatterjee1993@gmail.com>
2025-09-16 16:16:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "aNIzGS7Y#PxnGji8jMMLwRXk-a8yz_HyuHWLJWMJNi2hWuQjn_1Y", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "8420982819",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
 DEV: process.env.DEV || "94779062397",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
