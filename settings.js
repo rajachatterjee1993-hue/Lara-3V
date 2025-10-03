5c85c69278404057a67f0eecea7fc2858525b28d
rajachatterjee1993-hue <rajachatterjee1993@gmail.com>
2025-10-04 00:23:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aNIzGS7Y#PxnGji8jMMLwRXk-a8yz_HyuHWLJWMJNi2hWuQjn_1Y", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "XA4ETCgD#AhQg4IMk1JADxQi4xiktLo18GlB-Arl0i3xFgkz_sd8", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
